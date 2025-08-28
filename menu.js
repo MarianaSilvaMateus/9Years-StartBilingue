53283f9e831292b3f841e5a8c215f3c942160251
MarianaSilvaMateus <80151881+MarianaSilvaMateus@users.noreply.github.com>
2025-08-28 01:31:55 -0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,33 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+	menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+	menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+	menu.classList.remove('abrir-menu')
+})
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+	proximaImg()
+}, 5000)
+
+function proximaImg(){
+	cont++
+
+	if(cont > 4){
+		cont = 1
+	}
+	document.getElementById('radio'+cont).checked = true
+}
